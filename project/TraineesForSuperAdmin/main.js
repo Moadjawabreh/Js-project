0bbdc48d84aebce38990e998418c07ab910bf5ef
Moad Jawabreh <moadjawabreh2001@gmail.com>
2023-11-25 16:44:56 +0300

    modifies

--- project/TraineesForSuperAdmin/main.js
+++ project/TraineesForSuperAdmin/main.js
@@ -1,22 +1,25 @@
-forShowingStudents=()=>{
+forShowingStudents=()=>
+{
     let tBody=document.getElementById("tbody");
     const students=JSON.parse(localStorage.getItem("students"));
-    let traineerId = JSON.parse(sessionStorage.getItem('traineerId'));
+    let trainerId = JSON.parse(sessionStorage.getItem('traineerId'));
     
     for (let student of students) {
         
-        if (student.supervisorId === traineerId) {
+        if (student.supervisorId === trainerId) {
             
             let tableRow=document.createElement("tr");
             tableRow.innerHTML=`<td>${student.id}</td>
-                                <td>${student.name}</td>
+                                <td>${student.studentName}</td>
                                 <td>${student.solvedTasks} <button></button></td>
-                                <td>${student.totalTasks}</td>
-                                <td>${student.obsence} <button></button></td>`;
+                                <td>${student.taskInput}</td>
+                                <td>${student.absent} <button></button></td>`;
             tBody.appendChild(tableRow);  
         }
     }
 }
+
+forShowingStudents();
 
 
 
